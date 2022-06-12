300b893ee7560d7248541a569d9d1b59fbff84a6
phantware <phantmoney2011@gmail.com>
2022-06-13 00:21:35 +0100

    project updated

--- src/Gui.jsx
+++ src/Gui.jsx
@@ -4,35 +4,58 @@ import Table from './Table'
 import { getCurrentQueryData } from './services/query'
 
 const Gui = (e) => {
-  const [query, setQuery] = useState('categories')
   const [data, setData] = useState({
     payload: null,
     loading: false,
     error: false,
   })
-
-  const handleQuery = async (query) => {
+  const handleQuery = async (query, whereKey, whereValue) => {
     console.log(query)
     try {
       setData({ loading: true })
-      const data = await getCurrentQueryData(query)
+      let data = await getCurrentQueryData(query)
+      if (whereKey)
+        data = data.filter((user) => `${user[whereKey]}` === `${whereValue}`)
+      if (!data.length) alert('No Entries Found!')
+
       setData({ payload: data })
     } catch (error) {
       console.log(error)
       setData({ error: error.message })
     }
   }
+
+  // textarea
+  const [textAreaValue, setTextAreaValue] = React.useState('')
+  const handleTextAreaValueChange = (e) => {
+    // don't change when '\n' value is changed
+    if (e.target.value.includes('\n')) return
+    setTextAreaValue(e.target.value)
+  }
+
+  const handleTextAreaValueSubmit = () => {
+    if (
+      !/^SELECT \* from (categories|customers|employees|orders|products|regions|shippers|suppliers)(\sWHERE [a-zA-Z0-9]+ = .+)?$/.test(
+        textAreaValue
+      )
+    )
+      return alert('Invalid Query or Syntax Error!')
+
+    const query = textAreaValue.split(' ')[3]
+    const whereKey = textAreaValue.split(' ')[5]
+    const whereValue = textAreaValue.split(' ')[7]
+    handleQuery(query, whereKey, whereValue)
+  }
+
   const resetQuery = (e) => {
     e.preventDefault()
-    setData(' ')
-  }
-  const submitQuery = (e) => {
-    e.preventDefault()
-    // setQuery()
+    setData([])
+    setTextAreaValue('')
+    setSelectedQuery('')
   }
 
   const [selectedQuery, setSelectedQuery] = React.useState('')
-  const handleChange = (event) => {
+  const handleSelectQueryChange = (event) => {
     setSelectedQuery(event.target.value)
   }
   useEffect(() => {
@@ -54,15 +77,15 @@ const Gui = (e) => {
         <div>
           <textarea
             name=''
-            // value={query}
+            value={textAreaValue}
             id=''
             cols='100'
             rows='10'
             className='textareaContainer'
-            onChange={(e) => setQuery(e.target.value)}
+            onChange={handleTextAreaValueChange}
           ></textarea>
           <br />
-          <button className='btn2' onClick={submitQuery}>
+          <button className='btn2' onClick={handleTextAreaValueSubmit}>
             SUBMIT
           </button>
           <button className='btn2' onClick={resetQuery}>
@@ -73,15 +96,13 @@ const Gui = (e) => {
             GET QUERY
           </button>
           <label>Select to from the list:</label>
-
-          <select value={selectedQuery} onChange={handleChange}>
+          <select value={selectedQuery} onChange={handleSelectQueryChange}>
             <option value=''>select</option>
             {selectQueries_data.map((opt) => (
               <option value={opt.value}>{opt.label}</option>
             ))}
           </select>
-
-          {data.payload && <Table payload={data.payload} />}
+          {data.payload?.length ? <Table payload={data.payload} /> : null}
         </div>
       </div>
     </div>
